b63acf5d5da4b7c2b0f2f9183dfa0a86f068a84f
huangke19 <huangkwell@163.com>
2018-02-09 22:38:44 +0800

    将item暂时保存到json

--- food_scrapy/food_scrapy/pipelines.py
+++ food_scrapy/food_scrapy/pipelines.py
@@ -5,7 +5,19 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+import json
+from scrapy.exceptions import DropItem
+
 
 class FoodScrapyPipeline(object):
+    
+    def __init__(self):
+        self.file = open('caipu.json', 'a')
+    
     def process_item(self, item, spider):
-        return item
+        if item['name']:
+            recipe = json.dumps(dict(item)) + '\n'
+            self.file.write(recipe)
+            return item
+        else:
+            raise DropItem("this recipe is not available %s" % item)

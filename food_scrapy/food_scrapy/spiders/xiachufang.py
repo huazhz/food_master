f4a2a6ec4ddbd1daba3270ba1d052628788e0a80
huangke19 <huangkwell@163.com>
2018-02-10 18:45:12 +0800

    编写了新的item解析代码

--- food_scrapy/food_scrapy/spiders/xiachufang.py
+++ food_scrapy/food_scrapy/spiders/xiachufang.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import datetime, socket
-from ..items import FoodScrapyItem
+from ..items import FoodScrapyItem, NutritionItem, RecipeStepItem
 from scrapy.loader import ItemLoader
 from scrapy.loader.processors import MapCompose
 
@@ -9,54 +9,55 @@ from scrapy.loader.processors import MapCompose
 class XiachufangSpider(scrapy.Spider):
     name = 'xiachufang'
     allowed_domains = ['xiachufang.com']
-    start_urls = ['http://www.xiachufang.com/recipe/1086136/',
-                  'http://www.xiachufang.com/recipe/102904002/',
-                  'http://www.xiachufang.com/recipe/102325967/', ]
+    start_urls = ['http://www.xiachufang.com/recipe/1086136/', ]
     
     def parse(self, response):
         """ This function parses a xiachufang page.
-        @url http://www.xiachufang.com/recipe/1086136/'
+        @url http://www.xiachufang.com/recipe/1086136/
         @returns items 1
         @scrapes name cook steps cover_img rate_score ingredients
         @scrapes url project spider server date
         """
-        l = ItemLoader(item=FoodScrapyItem(), response=response)
+        recipe_name = response.xpath('//h1[@itemprop="name"]/text()').extract()[0].strip()
         
-        # prime fields
-        l.add_xpath('steps', '//div[@class="steps"]')
-        l.add_xpath('cover_img', '//div/div/img/@src')
-        l.add_xpath('cook', '//span[@itemprop="name"]/text()')
-        l.add_xpath('ingredients', '//div[@class="ings"]')
-        l.add_xpath('rate_score', '//span[@itemprop="ratingValue"]/text()')
-        l.add_xpath('name', '//h1[@itemprop="name"]/text()', MapCompose(str.strip))
-        l.add_xpath('brief', '//div[@itemprop="description"]/text()', MapCompose(str.strip))
+        l1 = ItemLoader(item=FoodScrapyItem(), response=response)
         
+        l1.add_xpath('cover_img', '//div/div/img/@src')
+        l1.add_xpath('cook', '//span[@itemprop="name"]/text()')
+        l1.add_xpath('rate_score', '//span[@itemprop="ratingValue"]/text()')
+        l1.add_xpath('name', '//h1[@itemprop="name"]/text()', MapCompose(str.strip))
+        l1.add_xpath('brief', '//div[@itemprop="description"]/text()', MapCompose(str.strip))
         # housekeeping fields
-        l.add_value('url', response.url)
-        l.add_value('project', self.settings.get('BOT_NAME'))
-        l.add_value('spider', self.name)
-        l.add_value('server', socket.gethostname())
-        l.add_value('date', str(datetime.datetime.now()))
+        l1.add_value('url', response.url)
+        l1.add_value('project', self.settings.get('BOT_NAME'))
+        l1.add_value('spider', self.name)
+        l1.add_value('server', socket.gethostname())
+        l1.add_value('date', str(datetime.datetime.now()))
         
-        yield l.load_item()
+        yield l1.load_item()
         
-        # old style
-        # item = FoodScrapyItem()
-        # item['name'] = response.xpath('//h1[@itemprop="name"]/text()').extract()[0].strip()
-        # item['cook'] = response.xpath('//span[@itemprop="name"]/text()').extract()[0].strip()
-        # item['brief'] = response.xpath('//div[@itemprop="description"]/text()').extract()[0].strip()
-        # item['cover_img'] = response.xpath("//div/div/img/@src").extract()[0]
-        # item['rate_score'] = response.xpath('//span[@itemprop="ratingValue"]/text()').extract()[0]
-        # item['ingredients'] = response.xpath('//div[@class="ings"]').extract()
-        # item['steps'] = response.xpath('//div[@class="steps"]').extract()
+        # parse the nutrition
+        nutrition = response.xpath('//div[@class="ings"]//tr')
         
-        # 打印
-        # self.log('rate_score: %s' % response.xpath('//span[@itemprop="ratingValue"]/text()').extract())
-        # self.log('name: %s ' % response.xpath('//h1[@itemprop="name"]/text()').extract())
-        # self.log('cook: %s ' % response.xpath('//span[@itemprop="name"]/text()').extract())
-        # self.log('brief: %s ' % response.xpath('//div[@itemprop="description"]/text()').extract())
-        # self.log('cover_img: %s' % response.xpath("//div/div/img/@src").extract())
-        # self.log('ingredients: %s' % response.xpath('//div[@class="ings"]').extract())
-        # self.log('steps: %s' % response.xpath('//div[@class="steps"]').extract())
+        for n in nutrition:
+            l2 = ItemLoader(item=NutritionItem(), response=response)
+            n_val = n.xpath('td[2]/text()').extract()[0].strip()
+            n_name = n.xpath('td[1]/a/text()').extract()[0].strip() if n.xpath('td[1]/a/text()').extract() else \
+                n.xpath('td[1]/text()').extract()[0].strip()
+            l2.add_value('name', n_name)
+            l2.add_value('val', n_val)
+            l2.add_value('recipe', recipe_name)
+            
+            yield l2.load_item()
         
-        # return item
+        # parse the recipe steps
+        steps = response.xpath('//div[@class="steps"]//li')
+        
+        for i, s in enumerate(steps):
+            l3 = ItemLoader(item=RecipeStepItem(), response=response)
+            l3.add_value('name', s.xpath('p/text()').extract()[0])
+            l3.add_value('image_url', s.xpath('img/@src').extract()[0])
+            l3.add_value('step_order', i + 1)
+            l3.add_value('recipe', recipe_name)
+            
+            yield l3.load_item()

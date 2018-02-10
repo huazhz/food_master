64fe30ff480c876a023ee7c9155b44e6d4c0b302
huangke19 <huangkwell@163.com>
2018-02-10 21:34:35 +0800

    parse the items

--- food_scrapy/food_scrapy/spiders/xiachufang.py
+++ food_scrapy/food_scrapy/spiders/xiachufang.py
@@ -11,14 +11,17 @@ class XiachufangSpider(scrapy.Spider):
     
     name = 'xiachufang'
     allowed_domains = ['xiachufang.com']
-    start_urls = ['http://www.xiachufang.com/recipe/1086136/', ]
+    start_urls = ['http://www.xiachufang.com/recipe/1086136/',
+                  'http://www.xiachufang.com/recipe/102416213/', ]
     
     def parse(self, response):
         """ This function parses a xiachufang page.
         @url http://www.xiachufang.com/recipe/1086136/
-        @returns items 1
-        @scrapes name cook steps cover_img rate_score ingredients
+        @returns items 3
+        @scrapes name cook cover_img rate_score
         @scrapes url project spider server date
+        @scrapes name val recipe
+        @scrapes name image_url step_order recipe
         """
         recipe_name = response.xpath('//h1[@itemprop="name"]/text()').extract()[0].strip()
         
@@ -33,10 +36,10 @@ class XiachufangSpider(scrapy.Spider):
         l1.add_xpath('brief', '//div[@itemprop="description"]/text()', MapCompose(str.strip))
         # - housekeeping fields -
         l1.add_value('url', response.url)
-        l1.add_value('project', self.settings.get('BOT_NAME'))
         l1.add_value('spider', self.name)
         l1.add_value('server', socket.gethostname())
         l1.add_value('date', str(datetime.datetime.now()))
+        l1.add_value('project', self.settings.get('BOT_NAME'))
         
         yield l1.load_item()
         
@@ -46,10 +49,11 @@ class XiachufangSpider(scrapy.Spider):
         for n in nutrition:
             l2 = ItemLoader(item=NutritionItem(), response=response)
             n_val = n.xpath('td[2]/text()').extract()[0].strip()
-            n_name = n.xpath('td[1]/a/text()').extract()[0].strip() if n.xpath('td[1]/a/text()').extract() else \
-                n.xpath('td[1]/text()').extract()[0].strip()
-            l2.add_value('name', n_name)
+            n_name = n.xpath('td[1]/a/text()').extract()[0].strip() \
+                if n.xpath('td[1]/a/text()').extract() \
+                else n.xpath('td[1]/text()').extract()[0].strip()
             l2.add_value('val', n_val)
+            l2.add_value('name', n_name)
             l2.add_value('recipe', recipe_name)
             
             yield l2.load_item()
@@ -57,11 +61,11 @@ class XiachufangSpider(scrapy.Spider):
         # ----------- parse the recipe steps ---------
         
         steps = response.xpath('//div[@class="steps"]//li')
-        for i, s in enumerate(steps):
+        for i, s in enumerate(steps, 1):
             l3 = ItemLoader(item=RecipeStepItem(), response=response)
+            l3.add_value('step_order', i)
+            l3.add_value('recipe', recipe_name)
             l3.add_value('name', s.xpath('p/text()').extract()[0])
             l3.add_value('image_url', s.xpath('img/@src').extract()[0])
-            l3.add_value('step_order', i + 1)
-            l3.add_value('recipe', recipe_name)
             
             yield l3.load_item()

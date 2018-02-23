f87f55eee5d2fb295e348b3ef955378198816d64
Arnold Lee <418586403@qq.com>
2018-02-23 14:58:05 +0800

    20180223

--- food_web/urls.py
+++ food_web/urls.py
@@ -19,5 +19,7 @@ from front import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index)
+    path('', views.index),
+    path('generic/', views.generic),
+    path('elements/', views.elements),
 ]
--- front/views.py
+++ front/views.py
@@ -12,3 +12,15 @@ def index(req):
     """ 首页 """
 
     return render(req,'front/index.html')
+
+
+def generic(req):
+    """ 首页 """
+
+    return render(req,'front/generic.html')
+
+
+def elements(req):
+    """ 首页 """
+
+    return render(req,'front/elements.html')

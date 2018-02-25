06841eb8b098025e4310e5062417ff93904b0b87
huangke19 <huangkwell@163.com>
2018-02-26 03:22:53 +0800

    success

--- food_scrapy/food_scrapy/settings.py
+++ food_scrapy/food_scrapy/settings.py
@@ -98,7 +98,7 @@ ITEM_PIPELINES = {
 # 爬到10个页面后停止
 # CLOSESPIDER_PAGECOUNT=10
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-# USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 USER_AGENTS = [
@@ -167,6 +167,6 @@ USER_AGENTS = [
     "Mozilla/5.0 (Linux; U; Android 1.6; en-us; SonyEricssonX10i Build/R1AA056) AppleWebKit/528.5  (KHTML, like Gecko) Version/3.1.2 Mobile Safari/525.20.1",
 ]
 
-# from .crawl_free_ip import get_ips
+from .crawl_free_ip import get_ips
 
-# IPLIST = get_ips()
+IPLIST = get_ips()

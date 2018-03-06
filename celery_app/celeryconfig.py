04c7ed214e5bb1da2a8f938e0cc12abb77d25d00
huangke19 <huangkwell@163.com>
2018-03-07 02:53:43 +0800

    使用RabbitMQ代替Redis

--- celery_app/celeryconfig.py
+++ celery_app/celeryconfig.py
@@ -5,7 +5,8 @@ from datetime import timedelta
 from celery.schedules import crontab
 
 # Broker and Backend
-BROKER_URL = 'redis://127.0.0.1:6379'
+# BROKER_URL = 'redis://127.0.0.1:6379'
+BROKER_URL = 'pyamqp://guest@localhost//'
 CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/0'
 # Timezone
 CELERY_TIMEZONE = 'Asia/Shanghai'  # 指定时区，不指定默认为 'UTC'
@@ -17,7 +18,7 @@ CELERY_IMPORTS = (
     'celery_app.spider_task',
     'celery_app.sql_task',
     # 'celery_app.ip_task',
-    'celery_app.hello'
+    # 'celery_app.hello'
     # 'spider_task',
     # 'sql_task',
     # 'ip_task'
@@ -34,7 +35,7 @@ CELERYBEAT_SCHEDULE = {
     #     'task': 'celery_app.hello.hello',
     #     'schedule': timedelta(seconds=10),  # 每 30 秒执行一次
     #     'schedule': crontab(hour=1, minute=22)
-        # 'args': None  # 任务函数参数
+    # 'args': None  # 任务函数参数
     # },
     '定时启动 sql-worker': {
         'task': 'celery_app.sql_task.save_2_mysql',
@@ -43,11 +44,10 @@ CELERYBEAT_SCHEDULE = {
     },
     '定时启动spider': {
         'task': 'celery_app.spider_task.start_spider',
-        'schedule': crontab(hour=0, minute=23),
-        # 'args': None  # 任务函数参数
+        'schedule': crontab(),
     },
-    'multiply-at-some-time': {
-        'task': 'celery_app.hello.hello',
-        'schedule': crontab(),  # 每天早上 9 点 50 分执行一次
-    }
+    # 'multiply-at-some-time': {
+    #     'task': 'celery_app.hello.hello',
+    #     'schedule': crontab(),  # 每天早上 9 点 50 分执行一次
+    # }
 }

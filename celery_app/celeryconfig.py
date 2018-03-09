d704deb14131852ee70a9fa8c9db367df74b1d32
huangke19 <huangkwell@163.com>
2018-03-09 22:17:40 +0800

    20180309

--- celery_app/celeryconfig.py
+++ celery_app/celeryconfig.py
@@ -8,7 +8,6 @@ from celery.schedules import crontab
 # BROKER_URL = 'redis://127.0.0.1:6379'
 BROKER_URL = 'pyamqp://guest@localhost//'
 CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/0'
-
 # Timezone
 CELERY_TIMEZONE = 'Asia/Shanghai'  # 坑，不生效，你大爷
 # CELERY_TIMEZONE='UTC'
@@ -27,7 +26,7 @@ CELERY_IMPORTS = (
 CELERYBEAT_SCHEDULE = {
     '获取免费ip': {
         'task': 'celery_app.ip_task.get_free_ip',
-        'schedule': crontab(minute='*/3', hour=16),  # 每3分钟抓取一次IP
+        'schedule': crontab(minute='*/3', hour=16),  # 每3分钟执行一次，但仅限于0点到1点
     },
     '定时启动 sql-worker1': {
         'task': 'celery_app.sql_task.save_recipe_2_mysql',
@@ -41,7 +40,7 @@ CELERYBEAT_SCHEDULE = {
     },
     '定时启动spider': {
         'task': 'celery_app.spider_task.start_spider',
-        'schedule': crontab(minute='*%10', hour=16),  # 爬虫在凌晨12点，每10分钟执行一次，每次执行540秒，到1点停止
+        'schedule': crontab(minute='*/10', hour=16),  # 爬虫在凌晨12点，每10分钟执行一次，每次执行540秒，到1点停止
     },
     'hello test': {
         'task': 'celery_app.hello.hello',

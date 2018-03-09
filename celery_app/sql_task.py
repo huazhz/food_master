0eef8efc85cdf8c95e041e068ded0d37ed71a381
huangke <huangkwell@163.com>
2018-03-09 22:14:59 +0800

    把硬编码转换成动态获取

--- celery_app/sql_task.py
+++ celery_app/sql_task.py
@@ -2,14 +2,14 @@
 # -*- coding: utf-8 -*-
 
 
-import sys
-import os
-
 import os
 import sys
 import json
 
-sys.path.insert(0, '/Users/macbook/个人项目/food_master')
+# sys.path.insert(0, '/Users/macbook/个人项目/food_master')
+
+proj_path = os.path.dirname(os.path.dirname(__file__))
+sys.path.insert(0, proj_path)
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'food_web.settings'
 import django

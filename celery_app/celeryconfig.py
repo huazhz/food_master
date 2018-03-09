615849fbc31f4b335dd65776206ba17c067657dd
huangke <huangkwell@163.com>
2018-03-09 18:29:49 +0800

    配置好爬虫每晚12点定时抓取数据存入数据库，1点结束抓取

--- celery_app/celeryconfig.py
+++ celery_app/celeryconfig.py
@@ -27,24 +27,24 @@ CELERY_IMPORTS = (
 CELERYBEAT_SCHEDULE = {
     '获取免费ip': {
         'task': 'celery_app.ip_task.get_free_ip',
-        'schedule': crontab(minute=40, hour=17 - 8),  # 每3分钟执行一次
+        'schedule': crontab(minute='*/3', hour=16),  # 每3分钟抓取一次IP
     },
     '定时启动 sql-worker1': {
         'task': 'celery_app.sql_task.save_recipe_2_mysql',
-        'schedule': crontab(minute=40, hour=17 - 8),  # 每 10 分钟执行一次
+        'schedule': crontab(minute=0, hour=16),  # 在凌晨12点开启
         'args': ('v',)
     },
     '定时启动 sql-worker2': {
         'task': 'celery_app.sql_task.save_list_2_mysql',
-        'schedule': crontab(minute=40, hour=17 - 8),  # 每 10 分钟执行一次
+        'schedule': crontab(minute=0, hour=16),  # 在凌晨12点开启
         'args': ('v',),
     },
     '定时启动spider': {
         'task': 'celery_app.spider_task.start_spider',
-        'schedule': crontab(minute=40, hour=17 - 8),  # 每10分钟执行一次
+        'schedule': crontab(minute='*%10', hour=16),  # 爬虫在凌晨12点，每10分钟执行一次，每次执行540秒，到1点停止
     },
     'hello test': {
         'task': 'celery_app.hello.hello',
-        'schedule': crontab(minute=40, hour=17 - 8),  #
+        'schedule': crontab(minute=0, hour=16),  # 没什么用，测试用的
     }
 }

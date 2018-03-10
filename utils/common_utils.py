b8cdb2ed6a2ae055b14a7a3c8b582f3905823cdd
Arnold Lee <418586403@qq.com>
2018-03-10 17:24:33 +0800

    404, 500 页面

--- utils/common_utils.py
+++ utils/common_utils.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseServerError
 
-def handle_404(req):
+def handle_404(req, **kwargs):
 
     return HttpResponseNotFound(
         render(req,'common/404.html')
     )
 
 
-def handle_500(req):
+def handle_500(req, **kwargs):
 
     return HttpResponseServerError(
         render(req,'common/500.html')

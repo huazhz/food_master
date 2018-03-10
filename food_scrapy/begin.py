883e04fb3a94323ebb90211f8faa7bfbdf0e34a0
huangke <huangkwell@163.com>
2018-03-10 14:59:32 +0800

    virtual scrapy

--- food_scrapy/begin.py
+++ food_scrapy/begin.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-''' 此脚本用于在Pycharm中单步调试scrapy 和 启动spider'''
+'''
+此脚本用于在Pycharm中单步调试scrapy
+'''
 import os
 from scrapy import cmdline
 
-# 此处需要指定虚拟环境里的scrapy
+proj_path = os.path.dirname(os.path.dirname(__file__))
 
-scrapy_path = '.env/bin/scrapy'
+# 此处需要指定虚拟环境里的scrapy
+scrapy_path = proj_path + '/.env/bin/scrapy'
+
+print(scrapy_path)
 
 execuete_spider = "%s crawl manual" % scrapy_path
 cmdline.execute(execuete_spider.split())

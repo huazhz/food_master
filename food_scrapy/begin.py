e751ec13506ebc90966b2a30f4a6c41caca6f077
huangke <huangkwell@163.com>
2018-03-10 14:16:44 +0800

    将scrapy指定为虚拟环境的scrapy

--- food_scrapy/begin.py
+++ food_scrapy/begin.py
@@ -4,7 +4,14 @@
 '''
 此脚本用于在Pycharm中单步调试scrapy
 '''
-
+import os
 from scrapy import cmdline
 
-cmdline.execute("scrapy crawl manual".split())
+proj_path = os.path.dirname(os.path.dirname(__file__))
+# scrapy_path = proj_path + '.env/bin/scrapy'
+
+# 此处需要指定虚拟环境里的scrapy
+scrapy_path = proj_path + 'venv/bin/scrapy'
+
+execuete_spider = "%s crawl manual" % scrapy_path
+cmdline.execute(execuete_spider.split())

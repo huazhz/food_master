e3688397f479140521685acd0dcf52c1dafd3bbe
huangke19 <huangkwell@163.com>
2018-03-11 20:43:36 +0800

    20180311

--- static/js/style.js
+++ static/js/style.js
@@ -1,6 +1,9 @@
 app.ready(function() {
-$('.search-input').bind("click",function(){
-  $("p").slideToggle();
-});
-
+    $('.search-input').bind("keypress",function(event){
+        if(event.keyCode == "13") {
+            var keyword = this.value;
+            to_url = '/search/' + keyword + '/';
+            top.location.href = to_url;
+        }
+    });
 });

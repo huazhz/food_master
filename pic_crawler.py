102a48004d922ffec9c023146f702f4bb13f250a
huangke <huangkwell@163.com>
2018-03-13 04:07:37 +0800

    重构crawler

--- pic_crawler.py
+++ pic_crawler.py
@@ -36,48 +36,69 @@ def percentage(consumed_bytes, total_bytes):
 from front.models import Recipe
 
 
+def make_dir(path):
+    '''
+    make dir for pics
+    '''
+    if os.path.exists(path):
+        pass
+    else:
+        os.mkdir(path)
+
+
+def got_img(img_url):
+    '''
+    get the image body and type
+    '''
+    img_res = requests.get(img_url)
+    try:
+        img_data = Image.open(BytesIO(img_res.content))
+        img_type = img_data.format.lower()
+        return (img_data, img_type)
+    except OSError:
+        r.sadd('OSError_urls', img_url)
+        return 0
+
+
 def cdn_crawler():
     '''
     爬取图片保存到本地，再异步上传到OSS
     :return:
     '''
+    dir_path = './OSS_PICS/'
+    make_dir(dir_path)
+    
     recipes = Recipe.objects.all()
     x = int(input('enter the start id: '))
+    
     for recipe in recipes[x:]:
         cover_img_url = recipe.cover_img
-        cover_res = requests.get(cover_img_url)
-        try:
-            cover_data = Image.open(BytesIO(cover_res.content))
-        except OSError:
-            r.sadd('OSError_urls', recipe.cover_img)
+        cover_info = got_img(cover_img_url)
+        if cover_info:
+            cname_elements = (recipe.id, recipe.fid, cover_info[1])
+            cname = 'i%sf%scover.%s' % (cname_elements)
+            filepath1 = dir_path + cname
+            cover_info[0].save(filepath1)
+            upload_to_oss.delay(filepath1)
+            print(cname)
+        else:
             continue
-        cname_obj = (recipe.id, recipe.fid, cover_data.format.lower())
-        cname = 'i%sf%scover.%s' % (cname_obj)
-        print(cname)
-        # if not os.path.exists('./OSS_PICS'):
-        #     os.mkdir('./OSS_PICS')
-        filepath1 = './OSS_PICS/i%sf%scover.%s' % cname_obj
-        cover_data.save(filepath1)
-        upload_to_oss.delay(filepath1)
         
-        for order, step in enumerate(recipe.recipestep_set.all(), 1):
+        steps = recipe.recipestep_set.all()
+        
+        for order, step in enumerate(steps, 1):
             step_img = step.image_url
             if step_img != '暂无':
-                step_res = requests.get(step.image_url)
-                try:
-                    step_data = Image.open(BytesIO(step_res.content))
-                except OSError:
-                    r.sadd('OSError_urls', step_img)
-                    continue
-                sname_obj = (recipe.id, recipe.fid, order, step_data.format.lower())
-                sname = 'i%sf%ss%s.%s' % sname_obj
-                print(sname)
-                filepath2 = './OSS_PICS/i%sf%ss%s.%s' % sname_obj
-                step_data.save(filepath2)
-                upload_to_oss.delay(filepath2)
+                step_info = got_img(step_img)
+                if step_info:
+                    sname_obj = (recipe.id, recipe.fid, order, step_info[1])
+                    sname = 'i%sf%ss%s.%s' % sname_obj
+                    filepath2 = dir_path + sname
+                    step_info[0].save(filepath2)
+                    upload_to_oss.delay(filepath2)
+                    print(sname)
             else:
                 continue
-        
         r.set('ossid', recipe.id)
     return None
 

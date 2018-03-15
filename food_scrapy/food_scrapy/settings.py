9865ee9b737e5fb046d798cd9f9170db42e2e2eb
huangke <huangkwell@163.com>
2018-03-15 23:28:44 +0800

    加大delay

--- food_scrapy/food_scrapy/settings.py
+++ food_scrapy/food_scrapy/settings.py
@@ -40,7 +40,7 @@ DEPTH_PRIORITY = -10
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
 
-DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 5
 
 # The download delay setting will honor only one of:
 # CONCURRENT_REQUESTS_PER_DOMAIN = 16

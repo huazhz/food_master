350af57f59c351f622567da1a6d6cbaa92adb8d1
huangke <huangkwell@163.com>
2018-03-16 22:20:16 +0800

    webhook 持续集成

--- start_webhook.py
+++ start_webhook.py
@@ -9,7 +9,7 @@ from wsgiref.simple_server import make_server
 from webhook import application
 
 # 创建一个服务器，IP地址为空，端口是8000，处理函数是application:
-httpd = make_server('', 8000, application)
+httpd = make_server('', 443, application)
 print('Serving HTTP on port 8000...')
 # 开始监听HTTP请求:
 httpd.serve_forever()

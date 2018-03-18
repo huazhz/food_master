45ba773c30aad99bc046a2b71a4092cb4e3569de
huangke <huangkwell@163.com>
2018-03-18 17:37:29 +0800

    列表页三个界面分页完成

--- food_web/urls.py
+++ food_web/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('category/<int:id>/', views.category),
-    path('category/<int:id>/<int:page_num>/$', views.category),
+    path('category/<int:id>/<int:page_num>/', views.category),
     path('search/<str:key>/', views.search_result),
     path('search/<str:key>/<int:page_num>/', views.search_result),
     path('recipe/<int:id>/', views.recipe_details),

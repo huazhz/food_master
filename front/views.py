8eef838cc08c465247e24e4778842b8951471430
Arnold Lee <418586403@qq.com>
2018-03-19 01:15:04 +0800

    test

--- front/views.py
+++ front/views.py
@@ -93,7 +93,7 @@ def webhook(req):
     print('webhook is running!')
 
     # sh_file = '/home/www/food_master/webhook.sh'
-    # os.system('cd /home/www/food_master/ && sh ./webhook.sh')
+    # os.system('cd /home/www/food_master/ && sh ./webhssook.sh')
     msg = os.popen('sh /home/www/food_master/webhook.sh').read()
     data = {'status': 'ok', 'message': msg}
     return HttpResponse(json.dumps(data), content_type="application/json")

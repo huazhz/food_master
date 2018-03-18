8cb465537264804d472891d788060c038b310781
huangke <huangkwell@163.com>
2018-03-18 11:41:25 +0800

    给菜谱打星星的patch

--- food_scrapy/food_scrapy/spiders/xiachufangpatch.py
+++ food_scrapy/food_scrapy/spiders/xiachufangpatch.py
@@ -4,23 +4,29 @@
 
 import json
 import scrapy
-from celery_app import r, app
+from celery_app import r
 from ..items import PatchItem
 from front.models import Recipe
 from scrapy import Request
-from django.core.exceptions import ObjectDoesNotExist
-
-from scrapy.spidermiddlewares.httperror import HttpError
-from twisted.internet.error import DNSLookupError
 from twisted.internet.error import TimeoutError, TCPTimedOutError
+from patch_crawler import patch_xiachufang
+
+fids = []
+for rec in Recipe.objects.all():
+    fids.append(rec.fid)
+
+print(fids)
 
 
 class XiachufangpathSpider(scrapy.Spider):
     name = 'xiachufangpatch'
     handle_httpstatus_list = [404, 502]
-    fid_begin_flag = r.get('fid_begin_flag').decode('utf8')  # 爬取起始点
+    
+    r.set('begin', 0)
+    begin = int(r.get('begin'))
+    nowfid = fids[begin]
     allowed_domains = ['xiachufang.com']
-    start_urls = ['http://m.xiachufang.com/recipe/%s/' % fid_begin_flag]
+    start_urls = ['http://m.xiachufang.com/recipe/%s/' % nowfid]
     
     def parse(self, response):
         
@@ -30,36 +36,20 @@ class XiachufangpathSpider(scrapy.Spider):
         except IndexError:
             stars = 0
         
-        fid = self.fid_begin_flag
-        item['fid'] = fid
+        nowfid = fids[self.begin]
+        item['fid'] = nowfid
         item['stars'] = stars
         
         v = json.dumps(dict(item))
         patch_xiachufang.delay(v)
         
-        self.fid_begin_flag = int(self.fid_begin_flag) - 1
-        next_recipe_url = 'http://m.xiachufang.com/recipe/%s/' % self.fid_begin_flag
+        self.begin += 1
+        nextfid = fids[int(self.begin)]
+        next_recipe_url = 'http://m.xiachufang.com/recipe/%s/' % nextfid
+        r.set('begin', self.begin)
+        
         yield Request(next_recipe_url, errback=self.errback_patch, callback=self.parse)
     
     def errback_patch(self, failure):
         if failure.check(TimeoutError):
             print('-----------------Timeout Error ---------------')
-
-
-@app.task(name='food_scrapy.spiders.xiachufangpatch.patch_xiachufang')
-def patch_xiachufang(v):
-    print('start to patch the spider')
-    infos = json.loads(v)
-    fid = infos.get('fid')
-    print('the fid is %s' % fid)
-    try:
-        recipe_obj = Recipe.objects.get(fid=fid)
-        print('get the object')
-        recipe_obj.stars = infos.get('stars')
-        
-        recipe_obj.save()
-        if recipe_obj.stars == infos.get('stars') and recipe_obj.stars != 0:
-            print('succeed updated stars number')
-    
-    except ObjectDoesNotExist:
-        pass

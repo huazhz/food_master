79ef71c0ebb268fdd4ac04b15a61c3325352d650
huangke <huangkwell@163.com>
2018-04-02 01:42:01 +0800

    三种不同的测试方法

--- front/tests.py
+++ front/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 
 # Create your tests here.
 
-from django.test import TestCase, Client
+from django.test import TestCase, Client, RequestFactory
 from django.http import HttpRequest
 
 from .views import index
@@ -13,10 +13,29 @@ from .models import Recipe, RecipeCategory, RecipeDetails, RecipeIngredient, Rec
 class TestIndexView(TestCase):
     def setUp(self):
         self.client = Client(enforce_csrf_checks=True)
+        self.factory = RequestFactory()
     
-    def test_index(self):
-        response = self.client.get('/')
-        assert response.status_code == 200
+    def test_index1(self):
+        ''' try Client '''
+        response1 = self.client.get('/')
+        self.assertEqual(response1.status_code, 200)
+    
+    def test_index2(self):
+        ''' try RequestFactory '''
+        response2 = self.factory.get('/')
+        print(self.factory.request().method)
+        print(self.factory.request().path)
+        print(self.factory.request().build_absolute_uri())
+        print(response2.path)
+        print(response2.environ)
+        print(response2.body)
+    
+    def test_index3(self):
+        ''' 尝试第3种测试方法 '''
+        request = HttpRequest()
+        response = index(request)
+        print(response.status_code)
+        print(response.content)
     
     def tearDown(self):
         pass

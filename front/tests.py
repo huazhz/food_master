073210f337cd79b3912448763b9da92685647084
huangke <huangkwell@163.com>
2018-04-02 00:51:42 +0800

    添加第一个测试

--- front/tests.py
+++ front/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+from django.test import TestCase, Client
+from django.http import HttpRequest
+
+from .views import index
+from .models import Recipe, RecipeCategory, RecipeDetails, RecipeIngredient, RecipeStep, RecipeTag, Member, \
+    CategoryType, MemberRecipeList, Ingredient, Nutrition
+
+
+class TestIndexView(TestCase):
+    def setUp(self):
+        self.client = Client(enforce_csrf_checks=True)
+    
+    def test_index(self):
+        response = self.client.get('/')
+        assert response.status_code == 200
+    
+    def tearDown(self):
+        pass
